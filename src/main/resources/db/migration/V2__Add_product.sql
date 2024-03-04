4fdeb70fb70c29ed6206c52ea9e05e59279da173
Stankovic Zeljko <zeljko.stankovic@unisys.com>
2024-03-04 17:08:12 +0100

    add example data

---
+++ src/main/resources/db/migration/V2__Add_product.sql
@@ -0,0 +1,6 @@
+INSERT INTO Product (name, price) VALUES ('Product 1', 19.99);
+INSERT INTO Product (name, price) VALUES ('Product 2', 29.99);
+INSERT INTO Product (name, price) VALUES ('Product 3', 39.99);
+INSERT INTO Product (name, price) VALUES ('Product 4', 19.99);
+INSERT INTO Product (name, price) VALUES ('Product 5', 29.99);
+INSERT INTO Product (name, price) VALUES ('Product 6', 39.99);
