9a63296ea32702fa8248323a7c04aa78a31686b9
Stankovic Zeljko <zeljko.stankovic@unisys.com>
2024-03-04 17:04:38 +0100

    added Product Entity

---
+++ src/main/java/ch/hftm/stankovic/module/Product.java
@@ -0,0 +1,23 @@
+package ch.hftm.stankovic.module;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Double price;
+}
---
+++ src/main/resources/db/migration/V1__Create_product_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    price NUMERIC(10,2)
+);
